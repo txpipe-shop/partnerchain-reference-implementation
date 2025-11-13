6db9ae7c153bda78b9ebca8bbf18c43afd54b4d7
Francisco Joray <franjoray@gmail.com>
2025-11-13 17:31:12 -0300

    remove initial ship UTxOs in genesis

--- runtime/src/genesis.rs
+++ runtime/src/genesis.rs
@@ -21,10 +21,6 @@ pub const GENESIS_DEFAULT_JSON: &str = r#"
                     {
                         "policy": "0298aa99f95e2fe0a0132a6bb794261fb7e7b0d988215da2f2de2005",
                         "assets": [ ["tokenA", 271000000], ["tokenB", 1123581321] ]
-                    },
-                    {
-                        "policy": "7ba97fb6e48018ef131dd08916939350c0ce7050534f8e51b5e0e3a4",
-                        "assets": [ ["PILOT0", 1], ["PILOT1", 1] ]
                     }
                     ],
             "datum": "820080"
@@ -38,37 +34,7 @@ pub const GENESIS_DEFAULT_JSON: &str = r#"
                         "assets": [ ["asteriaAdmin", 1] ]
                     }
                     ],
-            "datum": "D8799F02581C7BA97FB6E48018EF131DD08916939350C0CE7050534F8E51B5E0E3A4FF"
-        },
-        {
-            "address": "707ba97fb6e48018ef131dd08916939350c0ce7050534f8e51b5e0e3a4",
-            "coin": 314150000,
-            "value": [
-                    {
-                        "policy": "6a25ad5476105ac4a3784769cb93f92fd67a11932ef9a65a61abd1d6",
-                        "assets": [ ["FUEL", 30] ]
-                    },
-                    {
-                        "policy": "7ba97fb6e48018ef131dd08916939350c0ce7050534f8e51b5e0e3a4",
-                        "assets": [ ["SHIP0", 1] ]
-                    }
-                    ],
-            "datum": "D8799F27034553484950304650494C4F54301B0000018AE4FFE838FF"
-        },
-        {
-            "address": "707ba97fb6e48018ef131dd08916939350c0ce7050534f8e51b5e0e3a4",
-            "coin": 2000,
-            "value": [
-                    {
-                        "policy": "6a25ad5476105ac4a3784769cb93f92fd67a11932ef9a65a61abd1d6",
-                        "assets": [ ["FUEL", 25] ]
-                    },
-                    {
-                        "policy": "7ba97fb6e48018ef131dd08916939350c0ce7050534f8e51b5e0e3a4",
-                        "assets": [ ["SHIP1", 1] ]
-                    }
-                    ],
-            "datum": "D8799F00004553484950314650494C4F54311B00000196C625FAE0FF"
+            "datum": "D8799F00581C7BA97FB6E48018EF131DD08916939350C0CE7050534F8E51B5E0E3A4FF"
         },
         {
             "address": "706a25ad5476105ac4a3784769cb93f92fd67a11932ef9a65a61abd1d6",
